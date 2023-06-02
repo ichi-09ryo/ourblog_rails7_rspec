cfc9ba79b5ef898ecabdf3e991af271794067866
Komatsubara Masato <m-koma@nifty.com>
2023-06-02 13:54:15 +0900

    postモデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
-
+  validates :image, presence: true
+  
   belongs_to :user
   has_one_attached :image
 end
